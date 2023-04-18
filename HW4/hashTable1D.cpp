8b4ad93b32b36546a8c704e5c39d8db9c4c6b926
Tommy-Graves <109875296+Tommy-Graves@users.noreply.github.com>
2023-04-18 16:07:10 -0400

    stuff

--- HW4/hashTable1D.cpp
+++ HW4/hashTable1D.cpp
@@ -1,1 +1,50 @@
 #include "hashTable1D.h"
+
+hashTable1D::hashTable1D() {
+	items = 0;
+	arr = new int* [size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = nullptr;
+	}
+}
+
+int hashTable1D::hash(int* inVal) {
+	return(*inVal % size);
+}
+
+int hashTable1D::Insert(int* inVal) {
+	if (items == size) {
+		cout << "The table is full" << endl;
+		return -1;
+	}
+	int ret = 1;
+	int address = hash(inVal);
+	if (arr[address] == nullptr) {	// covers inserting with no collisions
+		arr[address] = inVal;
+		return ret;
+	}
+	else {
+		while (arr[address] != nullptr) { // while loop iterates until it finds a nullptr where inval can be inserted
+			address++;
+			ret++;
+		}
+		arr[address] = inVal;
+		return ret;
+	}
+}
+
+int hashTable1D::Find(int* inVal) {
+	int ret = 1;
+	int address = hash(inVal);
+
+	if (arr[address] != nullptr && *arr[address] == *inVal) {	// found without collisions
+		return ret;
+	}
+	else {
+		
+	}
+}
+
+int hashTable1D::GetLength() {
+	return items;
+}
--- HW4/hashTable1D.h
+++ HW4/hashTable1D.h
@@ -1,14 +1,25 @@
 #pragma once
+#include <iostream>
+
+using namespace std; 
+
 class hashTable1D
 {
 	public:
 		hashTable1D();
-		void Insert(int*);
-		int* Find(int*);
+		int Insert(int*);
 
+		int Find(int*);
+		int Remove(int*);
+
+		int GetLength();
+		void Print();
 
 		~hashTable1D();
 	private:
-
+		int** arr;
+		int items;
+		int hash(int*);
+		int size = 500;
 };
 
